f57bf84373854f2a622c0011a92b4a2d18f27d3a
Enrique <eimbertbc@gmail.com>
2019-08-14 00:57:11 +0200

    tabla ok

--- src/main/java/main/PruebasConWord.java
+++ src/main/java/main/PruebasConWord.java
@@ -4,9 +4,11 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.math.BigInteger;
 import java.util.List;
 
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.IndexedColors;
 import org.apache.poi.xwpf.usermodel.BodyElementType;
 import org.apache.poi.xwpf.usermodel.IBodyElement;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
@@ -15,9 +17,15 @@ import org.apache.poi.xwpf.usermodel.XWPFRun;
 import org.apache.poi.xwpf.usermodel.XWPFStyle;
 import org.apache.poi.xwpf.usermodel.XWPFStyles;
 import org.apache.poi.xwpf.usermodel.XWPFTable;
+import org.apache.poi.xwpf.usermodel.XWPFTableCell;
+import org.apache.poi.xwpf.usermodel.XWPFTableRow;
 import org.apache.xmlbeans.XmlException;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTTblBorders;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTTblPr;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.STBorder;
 import org.w3c.dom.stylesheets.DocumentStyle;
 
+import fr.opensagres.poi.xwpf.converter.core.Color;
 import funcionesWord.v2.BuscarCamposEnDocumento;
 import funcionesWord.v2.Constants;
 import funcionesWord.v2.FusionarDocumentoWord;
@@ -30,50 +38,33 @@ public class PruebasConWord {
 		LeerDocumentoWord documento1 = new LeerDocumentoWord();
 		LeerDocumentoWord documento2 = new LeerDocumentoWord();
 		
-		documento1.openDocument("C:\\Users\\EIB\\Desktop\\MC-MUTUAL Funcional\\Documentos prueba","MODELO_PLIEGO_PCAP.docx");
-		documento2.openDocument("C:\\Users\\EIB\\Desktop\\MC-MUTUAL Funcional\\Documentos prueba","Ejemplo documento para insertar.docx");
+		//documento1.openDocument("C:\\Users\\EIB\\Desktop\\MC-MUTUAL Funcional\\Documentos prueba","MODELO_PLIEGO_PCAP.docx");
+		documento2.openDocument("C:\\Users\\eimbe\\Desktop\\pruebas","prueba.docx");
 		
 		
 		XWPFDocument document = new XWPFDocument();
 		
-		int pos = 0;
-//		for(XWPFParagraph p: documento2.getDocument().getParagraphs()) {
-//			XWPFParagraph pTmp = document.createParagraph();
-//			document.setParagraph(p, pos);
-//			
-//			for (XWPFRun r : p.getRuns()) { 
-//				pTmp.addRun(r);
-//			}
-//			pos++;
-//		}
 		XWPFStyles estilo = documento2.getDocument().getStyles();
 		XWPFStyles estiloNuevoDoc = document.createStyles();
 		
 
-		
-	
-		
-		pos=0;
-		List<IBodyElement> listElements = documento1.getDocument().getBodyElements();
+		int pos = 0;
+		int numTables = 0;
+		List<IBodyElement> listElements = documento2.getDocument().getBodyElements();
 		for(IBodyElement element: listElements) {
-			XWPFParagraph pTmp = document.createParagraph();
 			if (element.getElementType() == BodyElementType.PARAGRAPH) {
+				document.createParagraph();
 				document.setParagraph((XWPFParagraph)element, pos++);
 			}else 
 				if(element.getElementType() == BodyElementType.TABLE) {
-					document.insertTable(pos++, (XWPFTable)element);
+					XWPFTable tbl = document.createTable();
+					document.setTable(numTables, (XWPFTable)element);
+					setTableBorder(tbl);
 			}
-
 		}
-		
-		
-			
-		
-		
-//		for (XWPFTable tbl : documento2.getDocument().getTables()) {
-//			document.insertTable(pos++, tbl);
-//			
-//		}
+
+	
+	
 		
 		FileOutputStream fileOutput = new FileOutputStream (Constants.OUT_PATH + File.separator + Constants.TEST_DOCUMENT_NAME);
 		document.write(fileOutput);
@@ -81,4 +72,18 @@ public class PruebasConWord {
 		document.close();
 		
 	}
+	
+	private static void setTableBorder(XWPFTable table) {
+		  
+		CTTblPr tblpro = table.getCTTbl().getTblPr();
+
+		CTTblBorders borders = tblpro.addNewTblBorders();
+		borders.addNewBottom().setVal(STBorder.SINGLE); 
+		borders.addNewLeft().setVal(STBorder.SINGLE);
+		borders.addNewRight().setVal(STBorder.SINGLE);
+		borders.addNewTop().setVal(STBorder.SINGLE);
+		borders.addNewInsideH().setVal(STBorder.SINGLE);
+		borders.addNewInsideV().setVal(STBorder.SINGLE);
+
+		}
 }
